64397e3f57d018c7331a25eb14ba8cc187fbdf63
inaridarkfox4231 <ry-ota-10035-2m@ka.tnc.ne.jp>
2019-05-07 11:21:15 +0900

    シェーダとかちょっと追記

--- src/script.js
+++ src/script.js
@@ -3,18 +3,20 @@
 onload = function(){
   // canvasエレメントの取得
   var c = document.getElementById("canvas");
-  c.width = 500;
+  c.width = 300;
   c.height = 300;
 
   // webGLコンテキストの取得
   var gl = c.getContext('webgl');
 
-  // 画面をクリアするためのメソッド：gl.clear
-  // gl.clear(gl.COLOR_BUFFER_BIT);
-  // gl.COLOR_BUFFER_BITに設定したい色を放り込んでその色でクリアする感じ
-  // gl.COLOR_BUFFER_BITに色を設定するメソッドがgl.clearColor(x, y, z, w) (これらは0~1のfloat)
-
-  // canvasを黒でクリアする
+  // canvasを初期化する色を設定する
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // canvasを初期化する際の深度を設定する
+  gl.clearDepth(1.0);
+
+  // canvas初期化(深さについての組み込み変数を追加)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  function
 }
---
+++ src/script_fs.js
@@ -0,0 +1,4 @@
+void main(void){
+  // 真っ白に塗りつぶすだけ
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
